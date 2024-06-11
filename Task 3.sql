1feb3839a613782fcac99da2f88db950297a0e24
karga07 <73552442+karga07@users.noreply.github.com>
2024-06-11 21:26:39 +0200

    Update Task 3.sql

--- Task 3.sql
+++ Task 3.sql
@@ -6,26 +6,28 @@ SELECT
 	year_product,
 	previous_price,
 	(avg_product_price - previous_price)/previous_price AS difference1
-	FROM( SELECT *,
-		LAG(AVG(avg_product_price), 1) OVER (ORDER BY product_name, year_product) AS previous_price
-		FROM t_Karolina_Gajduskova_project_SQL_primary_final tkgpspf  
-		GROUP BY product_name, year_product) AS subquery1
-WHERE year_product > 2006
-ORDER BY difference1 ASC;
+	FROM( 
+		SELECT *,
+			LAG(AVG(avg_product_price), 1) OVER (PARTITION BY product_name ORDER BY product_name, year_product) AS previous_price
+			FROM t_Karolina_Gajduskova_project_SQL_primary_final tkgpspf  
+			GROUP BY product_name, year_product) AS subquery1
+	WHERE (avg_product_price - previous_price)/previous_price IS NOT NULL
+	ORDER BY difference1 ASC;
 
----- differences in price over thw whole period  -----
+---- differences in price over the whole period  -----
 
 SELECT 
 	product_name,
 	SUM(difference1)
 FROM(
-SELECT *,
-	(avg_product_price - previous_price)/previous_price AS difference1
-	FROM( SELECT *,
+	SELECT *,
+		(avg_product_price - previous_price)/previous_price AS difference1
+	FROM( 
+		SELECT *,
 		LAG(AVG(avg_product_price), 1) OVER (ORDER BY product_name, year_product) AS previous_price
 		FROM t_Karolina_Gajduskova_project_SQL_primary_final tkgpspf 
 		GROUP BY product_name, year_product) AS subquery1
-WHERE year_product > 2006
-ORDER BY difference1 ASC) AS subquery2
+	WHERE year_product > 2006
+	ORDER BY difference1 ASC) AS subquery2
 GROUP BY product_name
 ORDER BY SUM(difference1) ASC;
