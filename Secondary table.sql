223e59e8ec9e555c0515fb82eb0da5ea9f0bf53b
karga07 <73552442+karga07@users.noreply.github.com>
2024-06-11 21:34:10 +0200

    Update Secondary table.sql

--- Secondary table.sql
+++ Secondary table.sql
@@ -1,16 +1,16 @@
 ----- Secondary table -----
 
 CREATE VIEW t_Karolina_Gajduskova_project_SQL_secondary_final AS 
-SELECT 
-	country,
-	year,
-	GDP,
-	population,
-	gini
-FROM economies 
-WHERE country IN(
-	SELECT country
-	FROM countries 
-	WHERE region_in_world LIKE '%Europe%')
-AND year < 2019
-AND year > 2005;
+	SELECT 
+		country,
+		year,
+		GDP,
+		population,
+		gini
+	FROM economies 
+	WHERE country IN(
+		SELECT country
+		FROM countries 
+		WHERE region_in_world LIKE '%Europe%')
+	AND year BETWEEN 2006 AND 2018;
+	
